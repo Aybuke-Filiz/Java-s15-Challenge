c00d499fb58ce6d2e5eff7a5fc9cf3810ae8458f
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-06-13 14:45:51 +0300

    güncelleme-2

--- src/com/workintech/Kutuphane/Main.java
+++ src/com/workintech/Kutuphane/Main.java
@@ -3,7 +3,7 @@ package com.workintech.Kutuphane;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+    Kutuphane kutuphane=new Kutuphane();
 
     }
 }
